b6365d3952ac977a926f5011aa0e8024db43ae26
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2020-12-28 11:40:25 +0300

    :)

--- script.js
+++ script.js
@@ -28,15 +28,45 @@ function renderUsers(responce) {
         title.textContent = users.name;
         const article = document.createElement("p");
         article.classList.add("card-text");
+        article.setAttribute('id', "output")
         title.addEventListener('mouseenter', (e) => {
             if (e.target) {
-                article.textContent = JSON.stringify(users);
-            } else {article.textContent = ''}
+                let data = responce;
+
+                function json2Table(json) {
+                    let cols = Object.keys(json[0]);
+                    let headerRow = cols
+                        .map(col => `<th>${col}</th>`)
+                        .join("");
+                    let rows = json
+                        .map(row => {
+                            let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
+                            return `<tr>${tds}</tr>`;
+                        })
+                        .join("");
+
+                    //build the table
+                    const table = `
+                                    <table>
+                                         <thead>
+                                               <tr>${headerRow}</tr>
+                                         <thead>
+                                         <tbody>
+                                              ${rows}
+                                         <tbody>
+                                         <table>`;
+
+                    return table;
+                }
+                output = document.getElementById('output')
+                e.target.innerHTML = json2Table(data)
+            }
         });
         title.addEventListener('mouseleave', (e) => {
             if (e.target) {
-                article.textContent = '';
-        }});
+                e.target.textContent = users.name;
+            }
+        });
         cardBody.appendChild(title);
         cardBody.appendChild(article);
         card.appendChild(cardBody);
@@ -46,5 +76,3 @@ function renderUsers(responce) {
 }
 
 getUsers(renderUsers);
-
-// https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
