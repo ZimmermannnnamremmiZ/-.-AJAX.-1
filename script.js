c107a5b9293c1c8260de7b1113c182f5863a03cd
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2020-12-28 17:35:51 +0300

    >:]

--- script.js
+++ script.js
@@ -29,48 +29,44 @@ function renderUsers(responce) {
         const article = document.createElement("p");
         article.classList.add("card-text");
         article.setAttribute('id', "output")
-        title.addEventListener('mouseenter', (e) => {
-            if (e.target) {
-                let data = responce;
-
-                function json2Table(json) {
+        title.addEventListener('click', function (e) {
+            if (e.currentTarget) {
+                function jsonToTable(json) {
                     let cols = Object.keys(json[0]);
                     let headerRow = cols
                         .map(col => `<th>${col}</th>`)
                         .join("");
+                        console.log(json)
                     let rows = json
+                        .filter(el => e.target.textContent === el.name)
                         .map(row => {
                             let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
                             return `<tr>${tds}</tr>`;
                         })
                         .join("");
-
-                    //build the table
                     const table = `
                                     <table>
-                                         <thead>
-                                               <tr>${headerRow}</tr>
+                                        <thead>
+                                                <tr>${headerRow}</tr>
                                          <thead>
                                          <tbody>
-                                              ${rows}
+                                                ${rows}
                                          <tbody>
-                                         <table>`;
-
+                                    <table>`;
                     return table;
                 }
-                output = document.getElementById('output')
-                e.target.innerHTML = json2Table(data)
+                e.target.innerHTML = jsonToTable(responce)
             }
         });
         title.addEventListener('mouseleave', (e) => {
-            if (e.target) {
-                e.target.textContent = users.name;
+            if (e.currentTarget) {
+                e.currentTarget.textContent = users.name;
             }
         });
         cardBody.appendChild(title);
         cardBody.appendChild(article);
         card.appendChild(cardBody);
-        fragment.appendChild(card);
+    fragment.appendChild(card);
     });
     countainer.appendChild(fragment);
 }
